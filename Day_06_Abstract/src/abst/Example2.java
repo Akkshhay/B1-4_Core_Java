cabb2912d7f19f76e25a25c8461958bf6bb37e32
BADARINATHA K N <BADARINATHA K N@DESKTOP-A8TUUAM>
2022-04-06 02:54:29 +0530

    First Commit

---
+++ Day_06_Abstract/src/abst/Example2.java
@@ -0,0 +1,17 @@
+package abst;
+
+public class Example2 {
+	final int a=100;
+	void display()
+	{
+		 int a=50;
+	}
+
+	public static void main(String[] args) {
+		Example2 e = new Example2();
+		e.display();
+		
+
+	}
+
+}
