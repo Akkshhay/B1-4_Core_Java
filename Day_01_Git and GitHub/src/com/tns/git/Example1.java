1dc42ae9743629267da59d33920125e6ef697814
BADARINATHA K N <BADARINATHA K N@DESKTOP-A8TUUAM>
2022-04-05 23:11:54 +0530

    First Commit

---
+++ Day_01_Git and GitHub/src/com/tns/git/Example1.java
@@ -0,0 +1,30 @@
+package com.tns.git;
+import java.util.Scanner;
+public class Example1 {
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+	    int first, second, add, subtract, multiply;
+	    float devide;
+	    Scanner sc = new Scanner(System.in);
+
+	    System.out.print("Enter Two Numbers : ");
+	    first = sc.nextInt();
+	    second = sc.nextInt();
+
+	    add = first + second;
+	    subtract = first - second;
+	    multiply = first * second;
+	    devide = (float) first / second;
+
+	    System.out.println("Sum = " + add);
+	    System.out.println("Difference = " + subtract);
+	    System.out.println("Multiplication = " + multiply);
+	    System.out.println("Division = " + devide);
+	  }
+	
+
+	}
+
+
