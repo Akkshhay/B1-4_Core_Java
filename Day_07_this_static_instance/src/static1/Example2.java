48ea02c0afe9d95d7fd540025a7b1277f2172d9b
BADARINATHA K N <BADARINATHA K N@DESKTOP-A8TUUAM>
2022-04-06 03:26:37 +0530

    First Commit

---
+++ Day_07_this_static_instance/src/static1/Example2.java
@@ -0,0 +1,14 @@
+package static1;
+
+public class Example2 {
+	static void print()
+	{
+		System.out.println("it's a beautiful evening");
+	}
+
+	public static void main(String[] args) {
+		print();
+
+	}
+
+}
