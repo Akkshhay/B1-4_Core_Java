996b9c5e1b871cf59d3f66d62a9952759d872e19
BADARINATHA K N <BADARINATHA K N@DESKTOP-A8TUUAM>
2022-04-06 02:42:34 +0530

    First Commit

---
+++ Assignment_Day_05/src/decisionmaking/Example1.java
@@ -0,0 +1,18 @@
+package decisionmaking;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		int a=11,b=7;
+		if(a>b)
+		{
+			System.out.print(a);
+		}
+		else
+		{
+			System.out.print(b);
+		}
+
+	}
+
+}
---
+++ Assignment_Day_05/src/looping/EnhancedForLoop.java
@@ -0,0 +1,15 @@
+package looping;
+
+public class EnhancedForLoop {
+
+	public static void main(String[] args) {
+		int c[]= {115,112};
+		for(int var:c)
+		{
+			System.out.print((char)var+" ");
+			
+		}
+
+	}
+
+}
