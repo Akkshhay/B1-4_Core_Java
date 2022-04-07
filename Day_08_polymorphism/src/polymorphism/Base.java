b06d38a6b922734494f23ee85db36d6b4d03806b
BADARINATHA K N <BADARINATHA K N@DESKTOP-A8TUUAM>
2022-04-07 23:55:45 +0530

    First Commit

---
+++ Day_08_polymorphism/src/polymorphism/Base.java
@@ -0,0 +1,24 @@
+package polymorphism;
+
+public class Base {
+	void method1()
+	{
+		System.out.println("Parent1");
+	}
+}
+class derived extends Base
+{
+	void method1()
+	{
+		System.out.println("Child1");
+	}
+
+	public static void main(String[] args) {
+		Base b=new Base();
+		b.method1();
+		derived d=new derived();
+		d.method1();
+
+	}
+
+}
---
+++ Day_08_polymorphism/src/polymorphism/Renault.java
@@ -0,0 +1,23 @@
+package polymorphism;
+
+public class Renault {
+	String color;
+	int speed;
+	Renault()
+	{
+		System.out.println("Bought renault in 2022");
+	}
+	Renault(String color,int speed)
+	{
+		this.color=color;
+		this.speed=speed;
+	}
+
+	public static void main(String[] args) {
+		Renault r = new Renault();
+		Renault k = new Renault("red",50);
+		System.out.println(k.color+" "+k.speed);
+
+	}
+
+}
